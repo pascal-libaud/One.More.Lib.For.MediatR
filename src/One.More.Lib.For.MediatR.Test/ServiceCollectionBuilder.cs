d3d051cf8c54d0039fd0400684ace41fdbb24f72
Pascal L <102244862+pascal-libaud@users.noreply.github.com>
2024-02-22 11:16:35 +0100

    New tests for MemoryCache PipelineBehavior. (#1)

--- src/One.More.Lib.For.MediatR.Test/ServiceCollectionBuilder.cs
+++ src/One.More.Lib.For.MediatR.Test/ServiceCollectionBuilder.cs
@@ -12,15 +12,29 @@ public static class ServiceCollectionBuilder
 
     public static IServiceCollection ConfigureMediatR(this IServiceCollection services)
     {
-        return services
-            .AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Hello>());
+        return services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Hello>());
     }
 
     public static IServiceCollection ConfigureFluentValidation(this IServiceCollection services, bool active = true)
     {
         if (active)
-            return services
-                .AddMediatRExtensions(cfg => cfg.AddFluentValidationSupport(new[] { typeof(Hello).Assembly }));
+            return services.AddMediatRExtensions(cfg => cfg.AddFluentValidationSupport(new[] { typeof(Hello).Assembly }));
+        else
+            return services;
+    }
+
+    public static IServiceCollection ConfigureMemoryCache(this IServiceCollection services, bool active = true)
+    {
+        if (active)
+            return services.AddMediatRExtensions(cfg => cfg.AddMemoryCacheSupport());
+        else
+            return services;
+    }
+
+    public static IServiceCollection ConfigurePerformanceLogger(this IServiceCollection services, bool active = true)
+    {
+        if (active)
+            return services.AddMediatRExtensions(cfg => cfg.AddPerformanceLoggerSupport(100));
         else
             return services;
     }
@@ -28,5 +42,11 @@ public static class ServiceCollectionBuilder
     public static IMediator GetMediator(this ServiceProvider serviceProvider)
     {
         return serviceProvider.GetRequiredService<IMediator>();
+    }
+
+    public static ServiceProvider GetRequiredService<T>(this ServiceProvider serviceProvider, out T service) where T : notnull
+    {
+        service = serviceProvider.GetRequiredService<T>();
+        return serviceProvider;
     }
 }
